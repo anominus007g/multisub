07421f0e863ee5023551385ca7970dcbb45653c9
anominus007g <162445957+anominus007g@users.noreply.github.com>
2024-03-13 17:40:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -28,7 +28,7 @@ OWNER_ID = int(os.environ.get("OWNER_ID", "1331434457"))
 PORT = os.environ.get("PORT", "8080")
 
 #Database 
-DB_URI = os.environ.get("mongodb+srv://kanishkagarg:<XY0rw2EtDwCrHTYi>@cluster0.6cirkqm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DB_URI = os.environ.get("mongodb+srv://kanishkagarg:<XY0rw2EtDwCrHTYi>@cluster0.6cirkqm.mongodb.net/?retryWrites=true&w=majority")
 DB_NAME = os.environ.get("DATABASE_NAME", "Cluser0")
 
 #force sub channel id, if you want enable force sub
@@ -43,7 +43,7 @@ TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 #start message
 START_MSG = os.environ.get("START_MESSAGE", "ʜᴇʟʟᴏ {first}\n\n ɪ ᴀᴍ ᴍᴜʟᴛɪ ғɪʟᴇ sᴛᴏʀᴇ ʙᴏᴛ , ɪ ᴄᴀɴ sᴛᴏʀᴇ ᴘʀɪᴠᴀᴛᴇ ғɪʟᴇs ɪɴ sᴘᴇᴄɪғɪᴇᴅ ᴄʜᴀɴɴᴇʟ ᴀɴᴅ ᴏᴛʜᴇʀ ᴜsᴇʀs ᴄᴀɴ ᴀᴄᴄᴇss ɪᴛ ғʀᴏᴍ sᴘᴇᴄɪᴀʟ ʟɪɴᴋ.")
 try:
-    ADMINS=[6056484807]
+    ADMINS=[1331434457]
     for x in (os.environ.get("ADMINS", "0").split()):
         ADMINS.append(int(x))
 except ValueError:
