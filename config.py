cb4a8e2ecb01f44ddef8d8f126e8bf3e78fc2811
anominus007g <162445957+anominus007g@users.noreply.github.com>
2024-03-13 17:36:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -31,7 +31,7 @@ OWNER_ID = int(os.environ.get("OWNER_ID", "24407770"))
 PORT = os.environ.get("PORT", "8030")
 
 #Database
-DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://kanishkagarg:<XY0rw2EtDwCrHTYi>@cluster0.6cirkqm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://kanishkagarg:<XY0rw2EtDwCrHTYi>@cluster0.6cirkqm.mongodb.net/?retryWrites=true&w=majority")
 DB_NAME = os.environ.get("DATABASE_NAME", "Cluster0")
 
 #force sub channel id, if you want enable force sub
@@ -43,8 +43,8 @@ TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 #start message
 START_MSG = os.environ.get("START_MESSAGE", "<b>ʙᴀᴋᴋᴀᴀᴀ!! {first}\n\n ɪ ᴀᴍ ғɪʟᴇ sᴛᴏʀᴇ ʙᴏᴛ, ɪ ᴄᴀɴ sᴛᴏʀᴇ ᴘʀɪᴠᴀᴛᴇ ғɪʟᴇs ɪɴ sᴘᴇᴄɪғɪᴇᴅ ᴄʜᴀɴɴᴇʟ ᴀɴᴅ ᴏᴛʜᴇʀ ᴜsᴇʀs ᴄᴀɴ ᴀᴄᴄᴇss ɪᴛ ғʀᴏᴍ sᴘᴇᴄɪᴀʟ ʟɪɴᴋ.</b>")
 try:
-    ADMINS=[24407770]
-    for x in (os.environ.get("ADMINS", "6056484807 ").split()):
+    ADMINS=[1331434457]
+    for x in (os.environ.get("ADMINS", "6950166258").split()):
         ADMINS.append(int(x))
 except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
