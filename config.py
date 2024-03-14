7cd52377cc0bcd0464c0fe749693c8c744375b61
anominus007g <162445957+anominus007g@users.noreply.github.com>
2024-03-14 09:57:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -31,7 +31,7 @@ OWNER_ID = int(os.environ.get("OWNER_ID", "24407770"))
 PORT = os.environ.get("PORT", "8030")
 
 #Database
-DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://kanishkagarg:<XY0rw2EtDwCrHTYi>@cluster0.6cirkqm.mongodb.net/?retryWrites=true&w=majority")
+DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://kanishkagarg:XY0rw2EtDwCrHTYi@cluster0.6cirkqm.mongodb.net/?retryWrites=true&w=majority")
 DB_NAME = os.environ.get("DATABASE_NAME", "Cluster0")
 
 #force sub channel id, if you want enable force sub
